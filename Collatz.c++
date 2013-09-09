d9da2634c3b93e671695b71d7e685c80d5c0194a
miguel del valle <delvalle@cs.utexas.edu>
2013-09-09 15:12:56 -0500

    Initial commit for collatz

---
+++ Collatz.c++
@@ -0,0 +1,55 @@
+// ----------------------------
+// projects/collatz/Collatz.c++
+// Copyright (C) 2013
+// Glenn P. Downing
+// ----------------------------
+
+// --------
+// includes
+// --------
+
+#include <cassert>  // assert
+#include <iostream> // endl, istream, ostream
+
+#include "Collatz.h"
+
+// ------------
+// collatz_read
+// ------------
+
+bool collatz_read (std::istream& r, int& i, int& j) {
+    r >> i;
+    if (!r)
+        return false;
+    r >> j;
+    assert(i > 0);
+    assert(j > 0);
+    return true;}
+
+// ------------
+// collatz_eval
+// ------------
+
+int collatz_eval (int i, int j) {
+    // <your code>
+    int v = 1;
+    assert(v > 0);
+    return v;}
+
+// -------------
+// collatz_print
+// -------------
+
+void collatz_print (std::ostream& w, int i, int j, int v) {
+    w << i << " " << j << " " << v << std::endl;}
+
+// -------------
+// collatz_solve
+// -------------
+
+void collatz_solve (std::istream& r, std::ostream& w) {
+    int i;
+    int j;
+    while (collatz_read(r, i, j)) {
+        const int v = collatz_eval(i, j);
+        collatz_print(w, i, j, v);}}
---
+++ Collatz.h
@@ -0,0 +1,64 @@
+// --------------------------
+// projects/collatz/Collatz.h
+// Copyright (C) 2013
+// Glenn P. Downing
+// --------------------------
+
+#ifndef Collatz_h
+#define Collatz_h
+
+// --------
+// includes
+// --------
+
+#include <iostream> // istream, ostream
+
+// ------------
+// collatz_read
+// ------------
+
+/**
+ * reads two ints into i and j
+ * @param r a  std::istream
+ * @param i an int by reference
+ * @param j an int by reference
+ * @return true if that succeeds, false otherwise
+ */
+bool collatz_read (std::istream&, int&, int&);
+
+// ------------
+// collatz_eval
+// ------------
+
+/**
+ * @param i the beginning of the range, inclusive
+ * @param j the end       of the range, inclusive
+ * @return the max cycle length in the range [i, j]
+ */
+int collatz_eval (int, int);
+
+// -------------
+// collatz_print
+// -------------
+
+/**
+ * prints the values of i, j, and v
+ * @param w a std::ostream
+ * @param i the beginning of the range, inclusive
+ * @param j the end       of the range, inclusive
+ * @param v the max cycle length
+ */
+void collatz_print (std::ostream&, int, int, int);
+
+// -------------
+// collatz_solve
+// -------------
+
+/**
+ * read, eval, print loop
+ * @param r a std::istream
+ * @param w a std::ostream
+ */
+void collatz_solve (std::istream&, std::ostream&);
+
+#endif // Collatz_h
